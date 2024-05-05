99ad8eeefc69520a90a881ae18dba6cdf5df576a
rmmitc <riley.mitchell@student.unimelb.edu.au>
2024-05-05 20:28:25 +1000

    changes made for newer data to be processed

--- main.py
+++ main.py
@@ -27,3 +27,26 @@ ratings_df = ratings_df[~ratings_df["User-ID"].isin(USER_ID_del)]
 books_df.to_csv("datasets_preprocessed\BX-Books_Processed.csv", index=False)
 users_df.to_csv("datasets_preprocessed/BX-Users_Processed.csv", index=False)
 ratings_df.to_csv("datasets_preprocessed/BX-Ratings_Processed.csv", index=False)
+
+
+
+
+# New Files
+new_books_df = pd.read_csv("COMP20008-A2-Data-Files\BX-NewBooks.csv")
+NEW_ISBN_del = process_books(df=new_books_df)
+new_users_df = pd.read_csv("COMP20008-A2-Data-Files\BX-NewBooksUsers.csv")
+NEW_USER_ID_del = process_users(df=new_users_df)
+new_ratings_df = pd.read_csv("COMP20008-A2-Data-Files\BX-NewBooksRatings.csv")
+
+# Remove Abnormal ISBN
+new_books_df = new_books_df[~new_books_df["ISBN"].isin(NEW_ISBN_del)]
+new_ratings_df = new_ratings_df[~new_ratings_df["ISBN"].isin(NEW_ISBN_del)]
+
+# Remove Abnormal USER_ID
+new_users_df = new_users_df[~new_users_df["User-ID"].isin(NEW_USER_ID_del)]
+new_ratings_df = new_ratings_df[~new_ratings_df["User-ID"].isin(NEW_USER_ID_del)]
+
+
+new_books_df.to_csv("datasets_preprocessed\BX-NewBooks_Processed.csv", index=False)
+new_users_df.to_csv("datasets_preprocessed/BX-NewBooksUsers_Processed.csv", index=False)
+new_ratings_df.to_csv("datasets_preprocessed/BX-NewBooksRatings_Processed.csv", index=False)
